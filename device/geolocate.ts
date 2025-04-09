a25be8a79a914c71b75251bf69487b8fcf60516c
Wolf Mermelstein <wolf@404wolf.com>
2025-04-09 13:50:57 -0400

    Remove old user workorders route

--- device/geolocate.ts
+++ device/geolocate.ts
@@ -17,23 +17,14 @@ export async function getLocation(): Promise<GeolocationResponse | null> {
   const apiKey = Deno.env.get("GOOGLE_GEOLOCATION");
   const body = { wifiAccessPoints };
 
-  try {
-    const response = await fetch(
-      `https://www.googleapis.com/geolocation/v1/geolocate?key=${apiKey}`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body),
-      },
-    );
+  const response = await fetch(
+    `https://www.googleapis.com/geolocation/v1/geolocate?key=${apiKey}`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    },
+  );
 
-    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-
-    const data: GeolocationResponse = await response.json();
-
-    return data;
-  } catch (error) {
-    console.error("Error:", error);
-    return null;
-  }
+  return await response.json() as GeolocationResponse;
 }
